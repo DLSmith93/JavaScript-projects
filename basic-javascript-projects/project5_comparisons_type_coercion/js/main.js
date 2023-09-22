87cda5e8cca4ba57a367754849954b69a17070e8
DLSmith93 <baumbor@hotmail.com>
2023-09-21 18:35:27 -0700

    Practicing JavaScript Logic

--- basic-javascript-projects/project5_comparisons_type_coercion/js/main.js
+++ basic-javascript-projects/project5_comparisons_type_coercion/js/main.js
@@ -10,3 +10,59 @@ function random_stuff(){
 
     
 }
+
+document.getElementById("values").addEventListener("click", values);
+function values(){
+
+    document.getElementById("value1").innerHTML = 0/0;
+    document.getElementById("value2").innerHTML = isNaN("This is a sting.");
+    document.getElementById("value3").innerHTML = isNaN("007");
+    document.getElementById("value4").innerHTML = (-3E310);
+    document.getElementById("value5").innerHTML = (3E310);
+}
+
+document.getElementById("bool"),addEventListener("click", bool);
+function bool(myEvent){
+
+    if(myEvent.target.id == "bool"){
+        document.write(10 > 2);
+        document.write("<br>");
+        document.write(10 < 2);
+        console.log(2 + 2)
+        console.log(3 == 5);
+        document.write("<br>");
+        document.write(10 == 10);
+        document.write("<br>");
+        document.write(3 == 10);
+    }
+}
+
+document.getElementById("compare").addEventListener("click", compare);
+function compare(){
+
+    num1 = 10;
+    num2 = 10;
+    num3 = 82;
+    num4 = "82";
+    string1 = "Magnus";
+    string2 = "Magnus";
+
+    document.write(num1 === num2);
+    document.write("<br>");
+    document.write(num3 === num4);
+    document.write("<br>");
+    document.write(string1 === string2);
+
+}
+
+document.getElementById("logic").addEventListener("click", logic);
+function logic(){
+
+    document.write(5 > 2 && 10 > 4);
+    document.write("<br>");
+    document.write(5 > 10 && 10 > 4);
+    document.write("<br>");
+    document.write(5 > 10 || 10 > 4);
+    document.write("<br>");
+    document.write(5 > 10 || 10 > 20);
+}
