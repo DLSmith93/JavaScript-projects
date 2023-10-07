dca607a03aabc29e8a1b1fa4b8eaee2563c6155e
DLSmith93 <baumbor@hotmail.com>
2023-10-06 20:30:51 -0700

    Break and Continue Statements

--- basic-javascript-projects/project_loops_arrays/js/main.js
+++ basic-javascript-projects/project_loops_arrays/js/main.js
@@ -31,3 +31,53 @@ function for_loop(){
     }
     document.getElementById("iteration2").innerHTML = content;
 }
+
+document.getElementById("return").addEventListener("click", math_function);
+function math_function(){
+
+    let x = addition();
+
+    function addition(){
+
+        return 5 + 6;
+    }
+
+    document.getElementById("output2").innerHTML = x;
+}
+
+document.getElementById("object").addEventListener("click", object_function);
+function object_function(){
+
+    let car = {
+        make: "Dodge ",
+        model: "viper ",
+        year: "2021 ",
+        color: "red ",
+        description: function (){
+            return "The car is a " + this.year + this.color + this.make + this.model;
+        }
+    };
+
+        document.getElementById("car-object").innerHTML = car.description();
+    
+}
+
+document.getElementById("break").addEventListener("click", break_continue);
+function break_continue(){
+
+    let text1 = "";
+    let text2 = "";
+
+    for(let i = 0; i < 10; ++i){
+        if(i === 5) {break};
+        text1 += "The number is " + i + "<br>"
+    }
+
+    for(let i = 0; i < 10; ++i){
+        if(i === 5) {continue};
+        text2 += "The number is " + i + "<br>"
+    }
+
+    document.getElementById("break-output1").innerHTML = text1;
+    document.getElementById("break-output2").innerHTML = text2;
+}
